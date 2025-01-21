2e729c45816fa15936fff98e56631a8bb8dd1eee
33gtt <hugo.mardie.1@educa.jcyl.es>
2025-01-21 13:28:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', function () { var links = document.querySelectorAll('a[href^="#"]'); for (var i = 0; i < links.length; i++) { links[i].addEventListener('click', function (event) { event.preventDefault(); var target = document.querySelector(this.getAttribute('href')); if (target) { target.scrollIntoView({ behavior: 'smooth' }); } }); } }); 
